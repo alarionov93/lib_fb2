25135fa84c898ff3dfbb709dba1be4cde29c3726
Alexandr Larionov <alarionov93@yandex.ru>
2021-08-20 20:32:57 +0500

    initial, got working code for create html with imgs from fb2

---
+++ get_text_from_fb2.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup as BS
+from PIL import Image
+from io import BytesIO
+
+import base64
+import sys
+
+work_path = sys.argv[1] # e.g. /Users/sanya/Downloads
+
+f = open('%s/binary_fb2.html')
+
+soup = BS(f.read(), 'lxml')
+imgs = soup.find_all('binary')
+
+for i in imgs:
+	img = Image.open(BytesIO(base64.b64decode(i.text)))
+	i.attrs['id']
+	img.save('%s/imgs/%s' % i.attrs['id'])
+
+# <image l:href="#image0.jpg"/>
+f1 = open('%s/fb2_as_html_test.html', 'r')
+soup1 = BS(f1.read(), 'lxml')
+imgs1 = soup1.find_all('image')
+for i_file in imgs1:
+	i_file.replaceWith('<img src="%s">' % i_file.attrs['l:href'].split('#')[1])
+f_last = open('%s/fb2_as_html_test.html', 'w').write(str(soup1))
+
